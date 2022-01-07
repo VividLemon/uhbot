7cd2366cf619208b5b3827596588b7f196c7a556
issayah <kwiksilver344@gmail.com>
2022-01-07 13:51:49 -0600

    challenges fix

--- .eslintrc.js
+++ .eslintrc.js
@@ -49,6 +49,7 @@ module.exports = {
 		'space-infix-ops': 'error',
 		'space-unary-ops': 'error',
 		'spaced-comment': 'error',
-		yoda: 'error'
+		yoda: 'error', 
+		"yoda": 'error'
 	}
 }
--- src/index.js
+++ src/index.js
@@ -1,45 +1,9 @@
-const { Client, Intents, Collection } = require('discord.js')
-const { readdirSync } = require('fs')
 const { join } = require('path')
+const {ShardingManager} = require('discord.js')
 require('dotenv').config()
 
-const client = new Client({ intents: [Intents.FLAGS.GUILDS] })
+const manager = new ShardingManager(join(__dirname, './bot.js'), {token: process.env.TOKEN})
 
-client.commands = new Collection()
-const commandFiles = readdirSync(join(__dirname, 'commands')).filter((file) => file.endsWith('.js'))
-for (const file of commandFiles) {
-	const path = join(__dirname, 'commands', file)
-	const command = require(path)
-	client.commands.set(command.data.name, command)
-}
+manager.on('shardCreate', shard => console.log(`Launched shard ${shard.id}`))
 
-client.once('ready', () => {
-	client.user.setActivity('/ slash commands', { type: 'WATCHING' })
-	console.log('Ready')
-})
-
-client.on('interactionCreate', async (interaction) => {
-	if (!interaction.isCommand()) return
-	const command = client.commands.get(interaction.commandName)
-	try {
-		await command.execute(interaction)
-	}
-	catch (err) {
-		console.error(err, interaction)
-		return await interaction.reply({ content: 'There was an error while trying to execute this command\nError was logged', ephemeral: true })
-	}
-})
-
-client.on('interactionCreate', async (interaction) => {
-	if (!interaction.isButton()) return
-	const command = client.commands.get(interaction.message.interaction.name)
-	try {
-		await command.buttonExecute(interaction)
-	}
-	catch (err) {
-		console.error(err, interaction)
-		return await interaction.reply({ content: 'There was an error while trying to execute this command\nError was logged', ephemeral: true })
-	}
-})
-
-client.login(process.env.TOKEN)
+manager.spawn()
