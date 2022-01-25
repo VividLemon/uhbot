20e1110fad561791f1f71293670a4f90756c8e7e
issayah <kwiksilver344@gmail.com>
2022-01-25 14:35:51 -0600

    chore remove console.log

--- src/plugins/keyv.js
+++ src/plugins/keyv.js
@@ -3,7 +3,6 @@ const { join } = require('path')
 
 // Important! Must have redis container
 const path = join(process.cwd(), 'sqlite', 'challenges.sqlite')
-console.log(path)
 const keyv = new Keyv(`sqlite://${path}`)
 keyv.on('error', (err) => console.error(`Keyv ${err}`))
 
